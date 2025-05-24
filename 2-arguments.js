32f0687a5ac48bf99a6a3b76f66100f6e88f0d5f
Leul Mekonnen <leulmekonnen049@gmail.com>
2025-05-24 13:13:01 +0300

    Show the passed argument

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,7 +1,5 @@
 if (process.argv.length <= 2) {
   console.log("No argument");
-} else if (process.argv.length === 3) {
-  console.log("Argument found");
 } else {
-  console.log("Arguments found");
+  console.log(process.argv.slice(2).join(" "));
 }
