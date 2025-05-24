e84a9592736d84de9051dc2ae5529f25771f6acb
Leul Mekonnen <leulmekonnen049@gmail.com>
2025-05-24 12:57:48 +0300

    final push

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,6 +1,7 @@
-const returnC = "C";
+const returnC = "C is fun";
 const returnP = "Python is cool";
 const returnD = "JavaScript is amazing";
+
 console.log(returnC);
 console.log(returnP);
 console.log(returnD);
