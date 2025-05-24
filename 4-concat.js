598b569ec45e585d27314dbd65e96013eac4b7a4
Leul Mekonnen <leulmekonnen049@gmail.com>
2025-05-24 13:48:18 +0300

    backtick added

--- 4-concat.js
+++ 4-concat.js
@@ -1,9 +1,9 @@
 const args = process.argv.slice(2);
 
 if (args.length < 1) {
-  console.log("undefined is undefined");
+  console.log(`undefined is undefined `);
+} else if (args.length == 1) {
+  console.log(`${args.slice(0, 1)} is undefined `);
 } else {
-  // Take the first two arguments and join them with ' is '
-  const result = args.slice(0, 2).join(" is ");
-  console.log(result);
+  console.log(args.slice(0, 2).join(" is "));
 }
