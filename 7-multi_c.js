776486bb4ca80f95b064faa3ce00119749375ebb
Leul Mekonnen <leulmekonnen049@gmail.com>
2025-05-24 14:18:30 +0300

    2 loggs accepted

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,16 +1,12 @@
 const args = process.argv.slice(2);
 const data = "C is fun";
 
-if (args.length < 1) {
+const count = Number(args[0]);
+
+if (isNaN(count)) {
   console.log("Missing number of occurrences");
 } else {
-  const count = Number(args[0]);
-
-  if (isNaN(count)) {
-    console.log("Not a number");
-  } else {
-    for (let i = 0; i < count; i++) {
-      console.log(data);
-    }
+  for (let i = 0; i < count; i++) {
+    console.log(data);
   }
 }
